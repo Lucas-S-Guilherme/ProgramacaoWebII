3f1dc4d3889c85728c250ef28bd5eb1e761784cf
Lucas Guilherme <lucassguilherme159@gmail.com>
2024-10-08 17:37:57 -0400

    Atividade 01

---
+++ 02-10/atividade01.js
@@ -0,0 +1,11 @@
+// Variáveis para armazenar as notas
+let nota1 = 8.5;
+let nota2 = 7.0;
+let nota3 = 9.5;
+let nota4 = 6.5;
+
+// Calculando a média
+let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// Exibindo a média
+console.log("A média do aluno é: " + media);
